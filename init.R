88916dc8632aebb4c275f0db53d4c08ab40bf0c0
pocharis <arinze6@gmail.com>
2021-12-12 04:37:49 +0000

    added widget

--- init.R
+++ init.R
@@ -5,7 +5,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("data.table", "ggcorrplot", "caret")
+my_packages = c("data.table", "ggcorrplot", "caret", "shinyWidgets")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
